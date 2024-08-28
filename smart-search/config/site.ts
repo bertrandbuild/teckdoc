47b27da0c624d5fbbe941862c63b2b29a416ee7b
YohanGH <YohanGH@proton.me>
2024-08-28 18:37:40 +0200

    Config initial

--- smart-search/config/site.ts
+++ smart-search/config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Documentation",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "Welcome to the documentation page.",
   mainNav: [
     {
       title: "Home",
@@ -11,8 +11,8 @@ export const siteConfig = {
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "https://twitter.com/",
+    github: "https://github.com/YohanGH",
+    docs: "https://www.YohanGH.github.io",
   },
 }
