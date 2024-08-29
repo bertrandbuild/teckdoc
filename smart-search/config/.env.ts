e85651c0a66b3522fdf9593dcfcc9c02134e0ba3
YohanGh <YohanGH@proton.me>
2024-08-29 18:28:15 +0200

    Add configuration env

---
+++ smart-search/config/.env.ts
@@ -0,0 +1,8 @@
+//export const IS_DEV=import.meta.env.DEV;
+export const IS_DEV=false;
+
+// Wallet private key (need some GAL from the Galadriel faucet)
+export const VITE_WALLET_PK = import.meta.env.VITE_WALLET_PK;
+
+// # Galadriel
+export const VITE_OPEN_AI_VISION_CONTRACT_ADDRESS = import.meta.env.VITE_OPEN_AI_VISION_CONTRACT_ADDRESS || "";
