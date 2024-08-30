c4876c8ee54f8682fd5174f14512c16863161212
YohanGh <YohanGH@proton.me>
2024-08-30 09:21:46 +0200

    Remove svg

--- smart-search/components/search/dialog-ia.tsx
+++ smart-search/components/search/dialog-ia.tsx
@@ -26,22 +26,6 @@ export default function DialogIA({ searchedInput }: { searchedInput: string }) {
           </span>
         </div>
       </div>
-      <div className="size-5">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="lucide lucide-chevron-right text-transparent"
-        >
-          <path d="m9 18 6-6-6-6"></path>
-        </svg>
-      </div>
     </div>
   )
 }
