6fe683e1b6d6870aca656ab6c7c208178647cb3f
Bertrand <bertrandrigal.pro@gmail.com>
2024-09-07 14:10:42 +0200

    update admins emails

--- smart-search/components/web3auth/web3auth-context.tsx
+++ smart-search/components/web3auth/web3auth-context.tsx
@@ -4,6 +4,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 import { IProvider } from "@web3auth/base";
 import { Web3Auth } from "@web3auth/modal";
 import { web3auth } from './web3auth-config';
+const ADMIN_EMAILS = process.env.NEXT_PUBLIC_ADMIN_EMAILS?.split(",") || [];
 
 interface Web3AuthContextType {
   provider: IProvider | null;
